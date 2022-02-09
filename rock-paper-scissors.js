1a73e7efa31dc55d64aabe5e3b772807ac4a4be2
dannyJackets <djcoats23@gmail.com>
2022-02-09 14:14:34 -0500

    completed functionality of r.p.s. game and barebones UI per odin project requirements. looks could be greatly improved.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,13 +1,9 @@
-//computerPlay function to have computer return rock, paper, scissors choice randomly
-//playerSelection function to handle case-insensitive user input of rock, paper, scissors
-    //use *prompt* to get input from user
-//write function *round* to play a single round using computerPlay and playerSelection
-//write function called *game* to play 5 rounds
-
 const COMPUTER_CHOICE = ["rock", "paper", "scissors"];  //constant choices for random pick by computer
 const RANDOM_MIN = 0;   //indices for COMPUTER_CHOICE array
 const RANDOM_MAX = 2;
-const ROUNDS = 5;   //number of rounds to play
+const ROUNDS = 9;   //number of rounds to play
+var playerWins = 0; //number of player round wins
+var computerWins = 0;   //number of computer round wins
 
 function computerPlay(){    //provides a random r, p, s choice for the computer
     return(COMPUTER_CHOICE[getRandomInt()]);
@@ -17,12 +13,11 @@ function getRandomInt(){    //provides a random number from RANDOM_MIN to RANDOM
     return(Math.floor(Math.random() * (RANDOM_MAX - RANDOM_MIN + 1) + RANDOM_MIN));
 }
 
-function playRound(userPick){   //plays a round of r,p,s and returns the outcome of the round win/lose. re-runs for tie.
+function playRound(userPick){   //plays a round of r,p,s and returns the outcome of the round win/lose.
     let computerPick = computerPlay();
     let outcome;
 
     if(computerPick == userPick){   //tie case, recurse round
-        //console.log("Tie! Replay the Round.")
         document.getElementById("message").innerHTML = "Tie! Replay the Round.";
         return;
     }
@@ -30,23 +25,17 @@ function playRound(userPick){   //plays a round of r,p,s and returns the outcome
             (userPick == "paper" && computerPick == "scissors") ||
             (userPick == "scissors" && computerPick == "rock")){
         outcome = "lose";
-        //console.log("The computer played " + computerPick + " and you played " + userPick + ". You lose.... :(");
         document.getElementById("message").innerHTML = "The computer played " + computerPick + " and you played " + userPick + ". You lose.... :(";
     }
     else{
         outcome = "win";
-        //console.log("The computer played " + computerPick + " and you played " + userPick + ". You win!");
         document.getElementById("message").innerHTML = "The computer played " + computerPick + " and you played " + userPick + ". You win!";
     }
 
     return(outcome);
 }
 
-function game(userPick){
-    
-    let playerWins = 0;
-    let computerWins = 0;
-    
+function game(userPick){ 
     let outcome = playRound(userPick);
     if(outcome == "win"){
         playerWins++;
@@ -56,13 +45,18 @@ function game(userPick){
     }
     document.getElementById("score").innerHTML = "The score is You: " + playerWins + ", Computer: " + computerWins;
     if(playerWins == Math.round(ROUNDS / 2)){
-        document.getElementById("message").innerHTML = "Congrats, you're better than the computer. Now go find some friends and stop playing alone. <br>Click another button to play again";
+        document.getElementById("message").innerHTML = "Congrats, you're better than the computer. Now go find some friends and stop playing alone.... <br>Click another button to play again";
+        playerWins = 0; 
+        computerWins = 0;
     }
     else if(computerWins == Math.round(ROUNDS /2)){
-        document.getElementById("message").innerHTML = "Sorry bud, the computer is superior. <br>Click another button to play again";
+        document.getElementById("message").innerHTML = "Sorry bud, the computer is superior. <br><i>Click another button to play again</i>";
+        playerWins = 0;
+        computerWins = 0;
     }
 }
 
+//user button event listeners
 document.getElementById("rock").addEventListener("click", function(){
     game("rock");
 });
@@ -73,4 +67,7 @@ document.getElementById("scissors").addEventListener("click", function(){
     game("scissors");
 });
 
+//initial message content settings
 document.getElementById("welcome").innerHTML = "Rock, paper, scissors vs. the RNG computer <br>Best " + Math.round(ROUNDS / 2) + " of " + ROUNDS + " rounds" + "<br>Good Luck!";
+document.getElementById("message").innerHTML = "";
+document.getElementById("score").innerHTML = "";
